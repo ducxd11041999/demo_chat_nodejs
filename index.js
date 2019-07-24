3be9b021592301c6f0dd5009c4bf977e77ca9963
= <=>
2019-07-24 11:06:23 +0700

    make it better

---
+++ index.js
@@ -0,0 +1,61 @@
+/* đây là file main của cả server*/
+//khai báo một số thông số cơ bản của node
+var express = require("express");
+var app = express();
+app.use(express.static("./public"));
+var server = require("http").Server(app);
+var io = require("socket.io")(server);
+app.set("view engine","ejs");
+app.set("views","./views" )
+
+/*code here*/0
+var arrOnline = []
+server.listen(3000,function()// kết nối server port 3000
+	{
+		console.log("connected <3");
+	});
+
+app.get("/",function(req, res)
+{
+	res.render("trangchu")
+})
+io.on("connection",function(socket)
+{
+	console.log("có người vừa kết nối : " + socket.id)
+	socket.on("CLIENT-SEND-UESERNAME", function(data)
+	{
+		if(arrOnline.indexOf(data)>=0)
+		{
+			socket.emit("SERVER-SEND-REG-FAIL"," ")
+		}
+		else
+		{
+			socket.Username= data;
+			arrOnline.push(data)
+			console.log("đăng ký thành công")
+			socket.emit("SERVER-SEND-REG-SUCCESSFULL", data)
+			io.sockets.emit("SERVER-SEND-LIST-USER",arrOnline)
+		}
+	})
+	socket.on("CLIENT-SEND-LOGOUT", function()
+	{
+		console.log(socket.Username + " đã thoát")
+		arrOnline.splice(arrOnline.indexOf(socket.Username),1);
+		socket.broadcast.emit("SERVER-SEND-LIST-USER",arrOnline)
+	})
+	socket.on("CHAT",function(data)
+	{
+		io.sockets.emit("S-CHAT",{nameu: socket.Username,cont:data});
+	})
+	socket.on("danggo",function()
+	{
+		var s=socket.Username +"đang gõ chữ";
+		socket.broadcast.emit("aigo", s)
+		socket.on("danggo-stop",function()
+		{
+			//console.log(socket.Username +" ngừng gõ");
+			socket.broadcast.emit("ngunggo")
+		})
+	})
+
+});
---
+++ public/xuly.js
@@ -0,0 +1,66 @@
+var socket = io("http://localhost:3000");///kết nối lên server
+$(document).ready(function()
+	{
+		$("#loginForm").show()
+		$("#Chatform").hide()
+		$("#btnRegister").click(function()
+		{
+			socket.emit("CLIENT-SEND-UESERNAME",$("#txtUsername").val())
+		})
+		$("#btnLogout").click(function()
+		{
+			//	alert(1);
+			socket.emit("CLIENT-SEND-LOGOUT","")
+			$("#loginForm").show(2000)
+			$("#Chatform").hide(1000)
+		})
+		$("#btnSend").click(function()
+		{
+			socket.emit("CHAT",$("#txtMessages").val())
+		})
+		$("#txtMessages").focusin(function()
+		{	
+			socket.emit("danggo")
+		})
+		$("#txtMessages").focusout(function()
+		{	
+			socket.emit("danggo-stop")
+		})
+	})
+socket.on("SERVER-SEND-REG-FAIL",function()
+{
+	alert("user name đã có người sử dụng ")
+})
+socket.on("SERVER-SEND-REG-SUCCESSFULL",function(data)
+{
+	alert("đăng ký thành công( "+data+" là tên trong phòng chat của bạn)");
+	$("#currentUser").html(data);
+	$("#loginForm").hide(1000)
+	$("#Chatform").show(2000)
+})
+
+socket.on("SERVER-SEND-LIST-USER", function(data)
+{
+	$("#boxContent").html("")
+	data.forEach(function(i)	
+	{
+		$("#boxContent").append("<div class='User'>"+i+"</div");
+	})
+})
+
+socket.on("S-CHAT",function(data)
+{
+	///alert("gửi được tin nhắn rồi")
+	$("#listMessages").append("<div class='ms'>" + data.nameu+ ":"+ data.cont + "</div>");
+})
+
+socket.on("aigo",function(data)
+{
+$("#thong_bao").html(data);
+})
+/*
+socket.on("ngunggo",function()
+{
+	alert(2)
+	$("#thongbao").html("")
+})*/
